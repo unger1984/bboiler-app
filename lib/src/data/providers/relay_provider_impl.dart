0593d36158dab8acdc2b83cc0b447de136c06fb8
Unger Andrey <unger1984@gmail.com>
2022-11-05 23:35:57 +0300

    Change GPIO LIB

--- lib/src/data/providers/relay_provider_impl.dart
+++ lib/src/data/providers/relay_provider_impl.dart
@@ -2,19 +2,21 @@ import 'dart:io';
 
 import 'package:bboiler/src/domain/providers/relay_provider.dart';
 import 'package:flutter_gpiod/flutter_gpiod.dart';
+import 'package:uuid/uuid.dart';
 
 class RelayProviderImpl extends RelayProvider {
   int _pin;
   late GpioLine _gpio;
   bool state = false;
+  final uuid = Uuid();
 
   RelayProviderImpl({required int pin}) : _pin = pin {
     final chip = FlutterGpiod.instance.chips.singleWhere(
       (chip) => chip.label == 'pinctrl-bcm2711',
       orElse: () => FlutterGpiod.instance.chips.singleWhere((chip) => chip.label == 'pinctrl-bcm2835'),
     );
-    _gpio = chip.lines[22];
-    _gpio.requestOutput(initialValue: false, consumer: '');
+    _gpio = chip.lines[_pin];
+    _gpio.requestOutput(initialValue: false, consumer: 'flutter_gpiod ${uuid.v4()}');
   }
 
   @override
